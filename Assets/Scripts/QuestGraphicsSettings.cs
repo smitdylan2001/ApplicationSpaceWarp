658a7e0719e889ce922252d029bdea28dd14159c
Dylan Smit <dylan.smit@hotmail.com>
2022-08-08 22:33:06 +0200

    Improved quest settings script

--- Assets/Scripts/QuestGraphicsSettings.cs
+++ Assets/Scripts/QuestGraphicsSettings.cs
@@ -2,22 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Supported refresh rates on Quest 2
+public enum RefreshRates
+{
+    r72 = 72,
+    r90 = 90,
+    r120 = 120
+}
+
+//Tools for Oculus Quest
 public class QuestGraphicsSettings : MonoBehaviour
 {
     [SerializeField] private bool _enableSpacewarpOnStart = true, _useDynamicFFR = false;
     [SerializeField] private OVRManager.FixedFoveatedRenderingLevel _ffrLevelOnStart = OVRManager.FixedFoveatedRenderingLevel.Off;
+    [SerializeField] private RefreshRates _standardFramerate = RefreshRates.r90;
 
     void Start()
     {
         //Set spacwarp
-        if(_enableSpacewarpOnStart) OVRManager.SetSpaceWarp(true);
+        if(_enableSpacewarpOnStart) SetSpaceWarp(true);
 
         //Set FFR. Recommended to enable Subsampled Layout in Oculus Android settings for better visuals
         if (OVRManager.fixedFoveatedRenderingSupported)
         {
-            OVRManager.fixedFoveatedRenderingLevel = _ffrLevelOnStart;
-            OVRManager.useDynamicFixedFoveatedRendering = _useDynamicFFR;
+            SetFFR(_ffrLevelOnStart, _useDynamicFFR);
         }
+
+        //Set framerate
+        SetRefreshRate(_standardFramerate);
 
         //Extra performance tools
         //OVRManager.suggestedCpuPerfLevel = OVRManager.ProcessorPerformanceLevel.SustainedHigh;
@@ -29,8 +41,14 @@ public class QuestGraphicsSettings : MonoBehaviour
         OVRManager.SetSpaceWarp(enable);
     }
 
-    static public void SetFFR(OVRManager.FixedFoveatedRenderingLevel ffrLevel)
+    static public void SetFFR(OVRManager.FixedFoveatedRenderingLevel ffrLevel, bool useDynamic = false)
     {
         OVRManager.fixedFoveatedRenderingLevel = ffrLevel;
+        OVRManager.useDynamicFixedFoveatedRendering = useDynamic;
+    }
+
+    static public void SetRefreshRate(RefreshRates refreshRate)
+    {
+        OVRPlugin.systemDisplayFrequency = (int)refreshRate;
     }
 }
